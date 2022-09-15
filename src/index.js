0e0d35d50d8cf30fa2af9d1704650bf8dc86bd82
Leonid <leonid@leonid-MacBook.local>
2022-09-15 15:29:56 +0200

    virgolette

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import Portraits from './Pages/Photographies/Portraits';
 import Reportage from './Pages/Photographies/Reportage';
 import Photography from './Pages/Photography';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Route, Routes, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import WallTextures from './Pages/Photographies/WallTextures';
 import Ukraine2022 from './Pages/Photographies/Ukraine2022';
 import Pahf from './Pages/Photographies/PostApocalypticHighFashion';
@@ -22,19 +22,19 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/ph" element={<Photography />} />
-        <Route path="/ph/abstract" element={<Abstract />} />
-        <Route path="/ph/architecture" element={<Architecture />} />
-        <Route path="/ph/architecture/walltextures" element={<WallTextures />} />
-        <Route path="/ph/fashion" element={<Fashion />} />
-        <Route path="/ph/pahf" element={<Pahf />} />
-        <Route path="/ph/portraits" element={<Portraits />} />
-        <Route path="/ph/reportage" element={<Reportage />} />
-        <Route path="/ph/reportage/ukraine2022" element={<Ukraine2022 />} />
-        <Route path="*" element={<Home />} />
+          <Route exact path='/' element={<Home/>} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/ph' element={<Photography />} />
+          <Route path='/ph/abstract' element={<Abstract />} />
+          <Route path='/ph/architecture' element={<Architecture />} />
+          <Route path='/ph/architecture/walltextures' element={<WallTextures />} />
+          <Route path='/ph/fashion' element={<Fashion />} />
+          <Route path='/ph/pahf' element={<Pahf />} />
+          <Route path='/ph/portraits' element={<Portraits />} />
+          <Route path='/ph/reportage' element={<Reportage />} />
+          <Route path='/ph//ukraine2022' element={<Ukraine2022 />} />
+          <Route path='*' element={<Home />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
